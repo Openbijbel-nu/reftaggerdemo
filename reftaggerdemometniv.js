980706581333af7b021b67cde8c50248a2b8cef7
OpenBijbel.nu Ontwikkelaar <Openbijbel.nu@gmail.com>
2014-10-19 23:37:38 +0200

    niet nodig voor wat ik wou.
    nu gelijk reftagger-niv benaderen!

--- reftaggerdemometniv.js
+++
@@ -1,6 +0,0 @@
-/**
- * This code is used to create ademo for reftagger
- */
-
-document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/Openbijbel-nu/reftaggerdemo/master/reftagger-niv.js';
-$("script:last").addClass("openbijbelreftaggerscript");
